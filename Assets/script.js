129199a3e78dd0b0ee77c5f80cb69f730f6f2b1c
jeffkorby <82245490+jeffkorby@users.noreply.github.com>
2021-11-04 14:04:35 -0400

    created js DOM elements and timezone plugins from dayjs

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,16 @@
 var openWeatherApiUrl = "https://api.openweathermap.org";
 var weatherAPIKey = "53acd474deefdc618c8086440c163ffc";
+var searchHistoryList = [];
+
+var searchHistory = document.querySelector("#history");
+var searchInput = document.querySelector("#search-input")
+var citySearch = document.querySelector("#city-search")
+var currentWeather = document.querySelector("#current-weather")
+var futureForecast = document.querySelector("#future-forecast")
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+
+
+
